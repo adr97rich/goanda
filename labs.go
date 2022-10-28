80a0d8672a484716beed97d81c9bde594636b938
Adrian Richiez <adr97rich@gmail.com>
2022-10-27 20:06:11 -0400

    Add GetSpreads function

--- labs.go
+++ labs.go
@@ -1,5 +1,13 @@
 package goanda
 
+type Spreads struct {
+
+  Max   [][]float64 `json:"max"`
+  Avg   [][]float64 `json:"avg"`
+  Min   [][]float64 `json:"min"`
+
+}
+
 func (c *OandaConnection) GetOrderBookData(instrument string, period string) interface{} {
 	var json_data interface{}
   endpoint := "labs/v1/orderbook_data?instrument=" + instrument + "&period=" + period
@@ -7,4 +15,14 @@ func (c *OandaConnection) GetOrderBookData(instrument string, period string) int
 	unmarshalJson(data, &json_data)
 
 	return json_data
+}
+
+func (c *OandaConnection) GetSpreads(instrument string, period string) Spreads {
+	var json_data interface{}
+  endpoint := "labs/v1/spreads?instrument=" + instrument + "&period=" + period
+	data := c.Request(endpoint)
+  spreads := Spreads{}
+	unmarshalJson(data, &spreads)
+
+	return spreads
 }
